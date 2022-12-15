15561327ba9223e7740a058de17910bad13bed41
Dmytro Smetanin <63594839+dmytrosmetanin@users.noreply.github.com>
2022-12-15 22:26:25 +0000

    Update businessman_task.py

--- businessman_task.py
+++ businessman_task.py
@@ -1,10 +1,6 @@
 import re
 from datetime import datetime
 
-dates = ('Sun 10:00-20:00\nFri 05:00-10:00\nFri 16:30-23:50\nSat 10:00-24:00\nSun 01:00-04:00\nSat 02:00-06:00\n'
-         'Tue 03:30-18:15\nTue 19:00-20:00\nWed 04:25-15:14\nWed 15:14-22:40\nThu 00:00-23:59\nMon 05:00-13:00\n'
-         'Mon 15:00-21:00')
-
 
 def solution(string):
     max_sleep = 0
@@ -29,4 +25,8 @@ def time_diff_mins(former, latter):
     return int(diff.seconds / 60)
 
 
-print(solution(dates))
+if __name__ == "__main__":
+    dates = ('Sun 10:00-20:00\nFri 05:00-10:00\nFri 16:30-23:50\nSat 10:00-24:00\nSun 01:00-04:00\nSat 02:00-06:00\n'
+             'Tue 03:30-18:15\nTue 19:00-20:00\nWed 04:25-15:14\nWed 15:14-22:40\nThu 00:00-23:59\nMon 05:00-13:00\n'
+             'Mon 15:00-21:00')
+    print(solution(dates))
